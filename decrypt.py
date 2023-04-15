19da38d36f045db40d8aa00a8645c2757b74fc1b
Bhea <bheaisipnobleza@gmail.com>
2023-04-16 00:43:49 +0800

    character * has already been change to a

--- decrypt.py
+++ decrypt.py
@@ -1,3 +1,14 @@
 # Input the encrypted string
 encrypted_str = input("The encrypted string is: ")
 
+# Use def to define the function and to decrypt
+def decrypt_str():
+    decrypted_str = ""
+
+# Check all the equivalent characters
+    for j in encrypted_str:
+    # If *, change to a
+        if j == "*":
+            decrypted_str += "a"
+        else:
+            decrypted_str += j
